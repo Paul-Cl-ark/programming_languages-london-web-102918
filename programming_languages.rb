78e02a237dc2c45a6649a1dac11d3d7b05423d9b
Paul Clark <clark.paul123@gmail.com>
2018-10-23 22:16:49 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,7 +5,8 @@ def reformat_languages(languages)
       languages_reformatted[name] = type
       languages_reformatted[name][:style] = []
       languages_reformatted[name][:style] << style
-      if new_hash[:javascript][:style] << :oo
+        if new_hash[:javascript][:style] << :oo
+      end
      end
   end
   languages_reformatted
