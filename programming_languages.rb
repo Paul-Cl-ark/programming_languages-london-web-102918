a2a3b00845d8c133097af233610fb36e69c0241b
Paul Clark <clark.paul123@gmail.com>
2018-10-23 22:12:06 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,7 @@ def reformat_languages(languages)
   languages_reformatted = {}
     languages.each do |style, language|
       language.each do |name, type|
-        languages_reformatted[name] = {type, :style => []}
+        languages_reformatted[name] = {type =>, :style => []}
         #languages_reformatted[name][:style] = []
         languages_reformatted[name][:style] << style
     end
